d36a3f118a0361ecd5a9760fabce840eae1094d1
Niris Okram <niris@amazon.com>
2023-10-13 20:17:55 +0000

    Migrate to Amazon Bedrock embedding and LLM ai21.j2-ultra-v1

--- lib/backend.py
+++ lib/backend.py
@@ -1,8 +1,8 @@
 import os
 from typing import Any, Dict, List
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms.bedrock import Bedrock
+from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -19,16 +19,42 @@ pinecone.init(
 )
 
 
+def get_llm():
+        
+    model_kwargs =  { #AI21
+        "maxTokens": 1024, 
+        "temperature": 0, 
+        "topP": 0.5, 
+        "stopSequences": ["Human:"], 
+        "countPenalty": {"scale": 0 }, 
+        "presencePenalty": {"scale": 0 }, 
+        "frequencyPenalty": {"scale": 0 } 
+    }
+    
+    llm = Bedrock(
+        credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"), #sets the profile name to use for AWS credentials (if not the default)
+        region_name=os.environ.get("BWB_REGION_NAME"), #sets the region name (if not the default)
+        model_id="ai21.j2-ultra-v1", #set the foundation model
+        model_kwargs=model_kwargs) #configure the properties for Claude
+    
+    return llm
+
+
+
 def ask_question(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    # embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = BedrockEmbeddings(
+        credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"),
+        region_name=os.environ.get("BWB_REGION_NAME"),
+        model_id = "amazon.titan-embed-text-v1"
+    )
     docsearch = Pinecone.from_existing_index(
         embedding=embeddings,
         index_name=INDEX_NAME,
     )
-    chat = ChatOpenAI(
-        verbose=True,
-        temperature=0,
-    )
+
+    chat = get_llm()
+
 
     qa = ConversationalRetrievalChain.from_llm(
         llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
--- lib/data_loader.py
+++ lib/data_loader.py
@@ -1,10 +1,15 @@
 # this is to embed data to Picone.
 
+# use the amazon bedrock embedding to Pinecone
+# dimension - 1536 cosine
+
+
 import os
+import boto3
 
 from langchain.document_loaders import S3DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -13,23 +18,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 INDEX_NAME = "blogs-embedding-vectors"
-S3_BUCKET_NAME = "genaiassistant"
-S3_PREFIX = "all_data/"
+S3_BUCKET_NAME = "genai-src-data"
+S3_PREFIX = "data"
 
 pinecone.init(
     api_key=os.environ.get("PINECONE_API_KEY"),
     environment=os.environ.get("PINECONE_ENVIRONMENT_REGION"),
 )
 
+# i had to locally modify the S3DirectoryLoader class to make it work with the AWS S3 bucket
+# https://github.com/langchain-ai/langchain/issues/6535
 
 def load_data():
-    loader = S3DirectoryLoader(S3_BUCKET_NAME, prefix=S3_PREFIX) # # https://github.com/langchain-ai/langchain/issues/6535
+    client = boto3.client('sts')
+    response = client.get_caller_identity()
+    print(response)
+    loader = S3DirectoryLoader(S3_BUCKET_NAME, prefix=S3_PREFIX)
     document = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
     )
     text = text_splitter.split_documents(document)
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = BedrockEmbeddings(
+        credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"),
+        region_name=os.environ.get("BWB_REGION_NAME"),
+        model_id = "amazon.titan-embed-text-v1"
+    )
     docsearch = Pinecone.from_documents(text, embeddings, index_name=INDEX_NAME)
     print("Data loaded to Pinecone")
 
